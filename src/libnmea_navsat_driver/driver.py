ed182f1f06dacc2b6ad2c72268a1a36c6fdcc8ef
yikben-wong-dx <103618314+yikben-wong-dx@users.noreply.github.com>
2025-05-28 11:41:59 +0900

    fix(driver): fix legacy numpy import

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -37,7 +37,6 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import NavSatFix, NavSatStatus, TimeReference
 from geometry_msgs.msg import TwistStamped, QuaternionStamped
-from tf_transformations import quaternion_from_euler
 from libnmea_navsat_driver.checksum_utils import check_nmea_checksum
 from libnmea_navsat_driver import parser
 
@@ -270,11 +269,11 @@ class Ros2NMEADriver(Node):
                 current_heading = QuaternionStamped()
                 current_heading.header.stamp = current_time
                 current_heading.header.frame_id = frame_id
-                q = quaternion_from_euler(0, 0, math.radians(data['heading']))
-                current_heading.quaternion.x = q[0]
-                current_heading.quaternion.y = q[1]
-                current_heading.quaternion.z = q[2]
-                current_heading.quaternion.w = q[3]
+                yaw = math.radians(data['heading'])
+                current_heading.quaternion.x = 0.0
+                current_heading.quaternion.y = 0.0
+                current_heading.quaternion.z = math.sin(yaw / 2.0)
+                current_heading.quaternion.w = math.cos(yaw / 2.0)
                 self.heading_pub.publish(current_heading)
         else:
             return False
